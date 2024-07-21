c6a61f094a9b8b30ba50e723465c7cd4c953403e
haseeb-ali-dev <h45.busy@gmail.com>
2024-07-21 18:01:45 +0500

    remove api folder & introduce api routes

---
+++ dinnerdashv2/api.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+
+api_router = DefaultRouter()
+
+# api_router.registry.extend(*[
+#     app_router.registry   # Here to include all apps router's registry
+# ])
